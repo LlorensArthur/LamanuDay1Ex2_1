5d710199b423fb084b99d447fc0ce67c263dca08
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:10:40 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿bool rainy = true;
+if (rainy)
+{
+    Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie");
+}
+else
+{
+    Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+}
